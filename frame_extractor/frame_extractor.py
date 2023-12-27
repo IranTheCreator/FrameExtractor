26a695b4d16b1fd06d7cd93fba5212804bf07d4c
Iranildo Alves <117860354+IranTheCreator@users.noreply.github.com>
2023-12-27 19:12:47 -0300

    Update frame_extractor.py

--- frame_extractor/frame_extractor.py
+++ frame_extractor/frame_extractor.py
@@ -1,3 +1,23 @@
+"""
+Video Frame Extractor
+
+This module provides a class for extracting frames from a video and saving them as images in a specified format.
+
+Usage:
+    from video_frame_extractor import VideoFrameExtractor
+
+    # Create an instance of VideoFrameExtractor
+    extractor = VideoFrameExtractor()
+
+    # Specify the path to the video file, output folder, and optional output format
+    video_path = "path/to/video.mp4"
+    output_folder = "path/to/output_folder"
+    output_format = "png"
+
+    # Extract frames from the video and save them in the specified format
+    extractor.extract_frames_from_video(video_path, output_folder, output_format)
+"""
+
 import cv2
 import face_recognition
 import os
@@ -60,3 +80,6 @@ class VideoFrameExtractor:
         # Release the video and close the process
         cap.release()
         cv2.destroyAllWindows()
+
+# Optionally, you can add more details to the documentation, such as an overview of the module, class, and methods.
+# You may also include examples of how to use the module.
